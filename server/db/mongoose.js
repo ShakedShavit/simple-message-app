374ab85b4f0eddeeafbb79dc8aaaa9d1df72f215
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-03-02 23:06:59 +0200

    changed MONGODB_URI to MONGODB_URL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //connecting to the database
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
